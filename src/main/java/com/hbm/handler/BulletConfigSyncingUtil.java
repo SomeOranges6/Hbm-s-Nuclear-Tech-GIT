a3c427a14331882292987ced3e235a1e66253f0e
70000hp <105080577+70000hp@users.noreply.github.com>
2022-07-25 15:30:45 -0600

    stuffs

--- src/main/java/com/hbm/handler/BulletConfigSyncingUtil.java
+++ src/main/java/com/hbm/handler/BulletConfigSyncingUtil.java
@@ -241,7 +241,6 @@ public class BulletConfigSyncingUtil {
 	public static int NUKE_MIRV_SAFE = i++;
 	public static int NUKE_MIRV_SPECIAL = i++;
 	
-	public static int MIRVLET = i++;
 
 	public static int NUKE_AMAT = i++;
 
@@ -514,7 +513,6 @@ public class BulletConfigSyncingUtil {
 		configSet.put(NUKE_MIRV_SAFE, GunFatmanFactory.getMirvSafeConfig());
 		configSet.put(NUKE_MIRV_SPECIAL, GunFatmanFactory.getMirvSpecialConfig());
 		
-		configSet.put(MIRVLET, GunFatmanFactory.getFullNukeConfig());
         
 		configSet.put(NUKE_AMAT, GunFatmanFactory.getBalefireConfig());
 
