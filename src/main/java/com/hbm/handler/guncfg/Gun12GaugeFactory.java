7f6f74e34979059a621e3f1af9ee33ada042020c
70000hp <105080577+70000hp@users.noreply.github.com>
2023-12-08 19:35:02 -0500

    e

--- src/main/java/com/hbm/handler/guncfg/Gun12GaugeFactory.java
+++ src/main/java/com/hbm/handler/guncfg/Gun12GaugeFactory.java
@@ -108,7 +108,6 @@ public class Gun12GaugeFactory {
 		config.comment.add("Alt-fire with Mouse 2 (Right-click) to fire 2 shells at once");
 		
 		config.config = HbmCollection.g12hs;
-
 		config.animations.put(AnimType.CYCLE, new BusAnimation()
 
 				.addBus("SPAS_RECOIL_TRANSLATE", new BusAnimationSequence()
